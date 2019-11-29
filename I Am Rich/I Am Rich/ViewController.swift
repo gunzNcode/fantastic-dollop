7c670e55b1668a18a17be57415882bbef5610793
gunzNcode <58303553+gunzNcode@users.noreply.github.com>
2019-11-29 13:21:02 -0500

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Moses Williams on 11/29/19.
+//  Copyright © 2019 Moses Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
